a68bfc187527ea93a2d5e24cde190ee221ac299c
pythonDev <daretimileyin1@gmail.com>
2024-08-22 10:58:45 +0100

    Delete projects/learn_fastapi directory

--- projects/learn_fastapi/test.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, school, age):
-        self.name = name
-        self.school = school
-        self.age = age
-
-value = Person(name="daretimileyin", school="futminna", age=23)
-
-print(value.__dict__) 
